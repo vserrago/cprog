6274641d9647f17f3bf46d151b6542d089ec83c7
Valentin Serrago <ogarres@gmail.com>
2012-11-04 21:53:42 -0500

    BST initial commit

---
+++ bst/bstTester.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "bst.h"
+
+
+int main(int argc, char * argv[])
+{
+    int i=0;
+    printf("%d\n", argc);
+
+    for (i=0; i<argc; i++)
+    {
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+    return 0;
+}
